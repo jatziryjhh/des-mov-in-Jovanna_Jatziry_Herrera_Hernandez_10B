f79df9dded5094b969cdd672ee80637ef25015ef
HerreraHernandezJovannaJatziry <20223tn062@utez.edu.mx>
2025-11-03 19:39:55 -0600

    [UPDATE] Se actualizo y se hizo el registro

--- login/lib/auth_errorrs.dart
+++ login/lib/auth_errorrs.dart
@@ -1,11 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
+
 String mapAuthErrorToMessage(Object e) {
   if (e is! FirebaseAuthException) return 'Correo o contraseña inválidos.';
   switch (e.code) {
     case 'invalid-email':
     case 'user-not-found':
     case 'wrong-password':
-      return 'Correo o contraseña inválidos.'; // mensaje genérico
+      return 'Correo o contraseña inválidos.'; // mensaje genérico para no filtrar info
+    case 'email-already-in-use':
+      return 'No se pudo crear la cuenta.'; // genérico: evita enumerar correos
+    case 'weak-password':
+      return 'La contraseña no cumple los requisitos.';
     case 'user-disabled':
       return 'La cuenta está deshabilitada.';
     case 'too-many-requests':
@@ -13,5 +18,5 @@ String mapAuthErrorToMessage(Object e) {
     case 'network-request-failed':
       return 'Problema de red. Verifica tu conexión.';
   }
-  return 'No se pudo iniciar sesión. Intenta de nuevo.';
+  return 'No se pudo completar la operación.';
 }
--- login/lib/validators.dart
+++ login/lib/validators.dart
@@ -7,10 +7,11 @@ class Validators {
     if (!emailRx.hasMatch(value)) return 'Correo inválido';
     return null;
   }
+
   static String? password(String? v) {
     final value = v ?? '';
-    if (value.length < 6) return 'Mínimo 6 caracteres';
-//    if (!RegExp(r'[A-Z]').hasMatch(value)) return 'Incluye al menos una mayúscula';
+    if (value.length < 8) return 'Mínimo 8 caracteres';
+    if (!RegExp(r'[A-Z]').hasMatch(value)) return 'Incluye al menos una mayúscula';
     if (!RegExp(r'[0-9]').hasMatch(value)) return 'Incluye al menos un número';
     return null;
   }
